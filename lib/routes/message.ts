c832e8ad6db412893d5d5f4c5c1fae5ac605f453
Rinat <lich666black@gmail.com>
2020-12-07 14:52:40 +0500

    added token balance validator

--- lib/routes/message.ts
+++ lib/routes/message.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import BN from 'bn.js';
 import fromentries from 'object.fromentries';
 import spaces from '@snapshot-labs/snapshot-spaces';
 import { verifySignature, pinJson } from '../utils';
 import { Message } from '../models';
-import { getBalances, getTotalSupply } from '../zilliqa';
+import { getBalances, getTotalSupply, getBalance } from '../zilliqa';
 
 import pkg from '../../package.json';
 
 import { ErrorCodes } from '../config';
+
+const _PROCENT = new BN(1);
 
 export const message = Router();
 
@@ -193,8 +196,21 @@ message.post('/message', async (req, res) => {
   let authorIpfsRes: any | null = null;
 
   if (msg.type === 'proposal') {
-    const balances = await getBalances(msg.token);
+    const createrBalance = await getBalance(msg.token, body.address);
     const totalSupply = await getTotalSupply(msg.token);
+    const _balance = new BN(createrBalance);
+    const _1000 = new BN(1000);
+    const _totalSupply = new BN(totalSupply);
+    const _n = _1000.mul(_PROCENT);
+    const _min = _totalSupply.div(_n);
+
+    if (_balance.lt(_min)) {
+      return res.status(400).json({
+        code: ErrorCodes.MIN_BALANCE_ERROR,
+        error_description: `Your balance below than 0.${_PROCENT}%.`
+      });
+    }
+    const balances = await getBalances(msg.token);
     authorIpfsRes = await pinJson({
       balances,
       totalSupply,
--- lib/zilliqa/balances.ts
+++ lib/zilliqa/balances.ts
@@ -1,8 +1,9 @@
 import { Zilliqa } from '@zilliqa-js/zilliqa';
 import { fromBech32Address } from '@zilliqa-js/crypto';
 
+const { blockchain } = new Zilliqa('https://api.zilliqa.com');
+
 export async function getBalances(token: string) {
-  const { blockchain } = new Zilliqa('https://api.zilliqa.com');
   const base16 = fromBech32Address(token);
   const field = 'balances';
   const res = await blockchain.getSmartContractSubState(
@@ -16,6 +17,27 @@ export async function getBalances(token: string) {
 
   if (res && res.result && res.result[field]) {
     return res.result[field];
+  }
+
+  return '0';
+}
+
+export async function getBalance(token: string, address: string) {
+  address = String(address).toLowerCase();
+  const base16 = fromBech32Address(token);
+  const field = 'balances';
+  const res = await blockchain.getSmartContractSubState(
+    base16,
+    field,
+    [address]
+  );
+
+  if (res.error) {
+    throw new Error(res.error.message);
+  }
+
+  if (res && res.result && res.result[field] && res.result[field][address]) {
+    return res.result[field][address];
   }
 
   return '0';
