4c74e9ae2e820dd5acc249d0d618f06cb426d181
Rinat <lich666black@gmail.com>
2021-02-09 17:31:28 +0500

    added Liquidity fetch

--- lib/zilliqa/balances.ts
+++ lib/zilliqa/balances.ts
@@ -1,7 +1,29 @@
 import { Zilliqa } from '@zilliqa-js/zilliqa';
 import { fromBech32Address } from '@zilliqa-js/crypto';
+import { cloneDeep } from 'lodash';
+import BN from 'bn.js';
 
 const { blockchain } = new Zilliqa('https://api.zilliqa.com');
+const zilswap = '0xBa11eB7bCc0a02e947ACF03Cc651Bfaf19C9EC00';
+
+export async function getPool(token: string) {
+  const field = 'balances';
+  const res = await blockchain.getSmartContractSubState(
+    zilswap,
+    field,
+    [token]
+  );
+
+  if (res.error) {
+    throw new Error(res.error.message);
+  }
+
+  if (!res || !res.result || !res.result[field] || !res.result[field][token]) {
+    return {};
+  }
+
+  return cloneDeep(res.result[field][token]);
+}
 
 export async function getBalances(token: string) {
   const base16 = fromBech32Address(token);
@@ -15,11 +37,30 @@ export async function getBalances(token: string) {
     throw new Error(res.error.message);
   }
 
-  if (res && res.result && res.result[field]) {
-    return res.result[field];
+  if (!res || !res.result || !res.result[field]) {
+    return {};
   }
 
-  return '0';
+  const balances = cloneDeep(res.result[field]);
+  let pool = {};
+
+  try {
+    pool = await getPool(String(base16).toLowerCase());
+  } catch {
+    //
+  }
+
+  for (const key in balances) {
+    if (key in pool) {
+      const _poolBalance = new BN(pool[key]);
+      const _tokenBalance = new BN(balances[key]);
+      const _balance = _poolBalance.add(_tokenBalance);
+
+      balances[key] = _balance.toString();
+    }
+  }
+
+  return balances;
 }
 
 export async function getBalance(token: string, address: string) {
